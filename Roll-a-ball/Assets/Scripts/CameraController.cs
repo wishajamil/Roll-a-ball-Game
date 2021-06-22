43bb03f37c0c70593464b12881c7185eb49f00a8
Wisha Jamil <45885029+wishajamil@users.noreply.github.com>
2021-06-22 14:13:04 -0400

    Add files via upload

---
+++ Roll-a-ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate runs every frame but will run after all of other updates done
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,83 @@
+﻿/*
+     unity12_wjamil1_Rollaball
+    
+     Wisha Jamil
+     Account: wjamil1@gamedev.cs.gsu.edu
+     CSc 4821
+     Unity PC12 Homework
+     Due date: 04/18/2021
+    
+     Description::
+         Make the player will roll around the plane collecting yellow objects. 
+	     every yellow cube is worth 10 points, if the player crashes with red traps/hazards,
+         the game will end.
+     Input:
+         Game inputs are the x and y movement of the player
+     Output:
+         Game output is the player moving using the up/down/left/right arrow keys
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+	public GameObject winTextObject;
+
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    private int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+		SetCountText();
+        winTextObject.SetActive(false);
+    }
+
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp")) 
+        {
+            other.gameObject.SetActive(false);
+			count = count + 10;
+			SetCountText();
+        }
+        if (other.gameObject.CompareTag("Trap")){
+	        countText.text = "Game Over!"; 
+            GetComponent<Rigidbody>().isKinematic = true;
+        }
+    }
+
+    void SetCountText()
+	{
+		countText.text = "Score: " + count.ToString();
+		if (count >= 120) 
+		{
+            winTextObject.SetActive(true);
+            GetComponent<Rigidbody>().isKinematic = true;
+		}
+	}
+}
